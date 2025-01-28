ea508493681c998bc35c3d6141cfa8bbd2ea4df7
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-28 15:41:02 -0300

    Agregar componente de misión y actualizar rutas y encabezado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MisionComponent } from './mision/mision.component';
 
 export const routes: Routes = [
+  { path: 'mision', component: MisionComponent},
    { path: '',  pathMatch: 'full', component: HomeComponent },
+    
 ];
