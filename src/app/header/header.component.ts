03057e66024d73401c0162aee1649612a4b3d27c
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-28 16:27:46 -0300

    Actualizar componente de encabezado para manejar colapsos y mejorar accesibilidad; modificar enlaces en la sección de inicio y actualizar contenido en la misión.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgbModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  isCollapsed = true;
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [  MarkdownModule  ],
+  imports: [  MarkdownModule, RouterModule  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
