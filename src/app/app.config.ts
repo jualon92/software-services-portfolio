747e7739e52c7fbfe32453bcbfc3a4e3373db84b
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-28 21:56:43 -0300

    Agregar botones de acción en el componente de inicio y actualizar estilos; incluir Bootstrap Icons en el proyecto.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { MarkdownModule, provideMarkdown } from 'ngx-markdown';
+import { provideRouter } from '@angular/router'; 
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideMarkdown(), ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration() ],
    
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { MarkdownModule } from 'ngx-markdown';
+ 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [  MarkdownModule, RouterModule  ],
+  imports: [  RouterModule  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
