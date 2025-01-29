13a7fb76a3654f24b05a8c50e509555fd702722a
Juan Ignacio Londra <londrajuan@gmail.com>
2025-01-28 21:12:50 -0300

    Agregar componente de contacto y actualizar rutas; modificar encabezado para incluir enlace a contacto.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MisionComponent } from './mision/mision.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
+  { path: 'contact', component: ContactComponent},
   { path: 'mision', component: MisionComponent},
    { path: '',  pathMatch: 'full', component: HomeComponent },
     
